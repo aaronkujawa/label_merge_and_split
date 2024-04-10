aea9b88cd4c4392df0b5241e60ec2c68772a8dd7
Aaron Kujawa <aku20@login.ib0.compute.bmeis-hpc.local>
2024-04-10 15:42:17 +0100

    Remove carriage returns in log_summary.sh to avoid issues with watch command

--- example_scripts/slurm/log_summary.sh
+++ example_scripts/slurm/log_summary.sh
@@ -7,6 +7,9 @@ if [ -z "$n_last" ]; then
     n_last=1
 fi
 
-echo -e "--------log.err-------------\n"; ls *.err | tail -n $n_last | head -n 1 | xargs cat | tail;  
-echo -e "\n\n--------log.out-----------------\n"; ls *.out | tail -n $n_last | head -n 1 | xargs cat | tail; 
-echo -e "\n-----------squeue------------\n"; squeue -u aku20
+echo -e "--------log.err-------------\n";
+ls *.err | tail -n $n_last | head -n 1 | xargs cat | tr '\r' '\n' | tail;  # tr removes carriage returns (for example from tqdm progress bar) because they cause problems with the watch command
+echo -e "\n\n--------log.out-----------------\n";
+ls *.out | tail -n $n_last | head -n 1 | xargs cat | tr '\r' '\n' | tail;
+echo -e "\n-----------squeue------------\n";
+squeue -u aku20
