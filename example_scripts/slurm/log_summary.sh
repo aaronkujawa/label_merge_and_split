34a6268720aa9c45cdc2ac97a99915af50dcfa17
Aaron Kujawa <aku20@login.ib0.compute.bmeis-hpc.local>
2024-04-11 08:36:44 +0100

    Fixed log_summary.sh

--- example_scripts/slurm/log_summary.sh
+++ example_scripts/slurm/log_summary.sh
@@ -7,9 +7,12 @@ if [ -z "$n_last" ]; then
     n_last=1
 fi
 
-echo -e "--------log.err-------------\n";
-ls *.err | tail -n $n_last | head -n 1 | xargs cat | tr '\r' '\n' | tail;  # tr removes carriage returns (for example from tqdm progress bar) because they cause problems with the watch command
-echo -e "\n\n--------log.out-----------------\n";
-ls *.out | tail -n $n_last | head -n 1 | xargs cat | tr '\r' '\n' | tail;
+err_file=`ls -rt *.err | tail -n $n_last | head -n 1`
+echo -e "--------log.err-------------" $err_file "\n";
+echo $err_file | xargs cat | tr '\r' '\n' | tail;  # tr removes carriage returns (for example from tqdm progress bar) because they cause problems with the watch command
+
+out_file=`ls -rt *.out | tail -n $n_last | head -n 1`
+echo -e "\n\n--------log.out-----------------" $out_file "\n";
+echo $out_file | xargs cat | tr '\r' '\n' | tail;
 echo -e "\n-----------squeue------------\n";
 squeue -u aku20
