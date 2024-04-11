e59692cd407dc6488ee27c807b6d0cf98671331c
Aaron Kujawa <aku20@login.ib0.compute.bmeis-hpc.local>
2024-04-11 12:24:52 +0100

    Cleaned slurm sbatch script

--- example_scripts/slurm/tom2_sbatch_script_taskid.sh
+++ example_scripts/slurm/tom2_sbatch_script_taskid.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition compute           # Submit to 'compute' Partition or queue
-#SBATCH --job-name=multiatlasprop
-#SBATCH --output=logs/run_multiatlasprop_array_%A_%a.out
-#SBATCH -e logs/run_multiatlasprop_array_%A_%a.err
-#SBATCH --time=0-72:00:00        # Run for a maximum time of 0 days, 72 hours, 00 mins, 00 secs
-#SBATCH --nodes=1            # Request N nodes
+#SBATCH --job-name=distmat
+#SBATCH --output=logs/run_distmat_array_%A_%a.out
+#SBATCH -e logs/run_distmat_array_%A_%a.err
+#SBATCH --time=0-72:00:00     # Run for a maximum time of 0 days, 72 hours, 00 mins, 00 secs
+#SBATCH --nodes=1             # Request N nodes
 #SBATCH --ntasks-per-node=64  # Request n cores or task per node
-#SBATCH --mem-per-cpu=3500M
+#SBATCH --mem-per-cpu=1000M
 
 # the environment variable SLURM_ARRAY_TASK_ID contains the index corresponding to the current job step
 
@@ -26,7 +26,7 @@ timestamp=$(date -d "today" +"%Y-%m-%d-%H_%M_%S")
 #python -m cProfile -o logs/profile_${timestamp}.dat ${script_path}
 python ${script_path} --taskid ${SLURM_ARRAY_TASK_ID}
 
-#if [[ $(((SLURM_ARRAY_TASK_ID+1) % nb_of_submitted_jobs_limit)) == 0 ]] ; then 
+#if [[ $(((SLURM_ARRAY_TASK_ID+1) % nb_of_submitted_jobs_limit)) == 0 ]] ; then
 #    sbatch --array=$((SLURM_ARRAY_TASK_ID+1))-$((SLURM_ARRAY_TASK_ID+nb_of_submitted_jobs_limit)) "$0" "$@"
 #fi
 
